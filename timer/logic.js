7c3e2dcf7a91d0244f89b0f42643aaea166b2aa8
Mir Saheb Ali <mirsahebali204@gmail.com>
2022-07-18 01:42:19 +0530

    Restarted

--- timer/logic.js
+++ timer/logic.js
@@ -1,72 +1,75 @@
-var minutes = 25;
-var seconds = "00";
-var sessions = 0;
-var click = new Audio("C:/Users/mirsa/OneDrive/Desktop/FocAce/click.wav");
-var bell = new Audio("C:/Users/mirsa/OneDrive/Desktop/FocAce/bell.wav");
-var bell2 = new Audio("C:/Users/mirsa/OneDrive/Desktop/FocAce/bell2.wav");
-function template() {
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
-  minutesTimer();
-  secondsTimer();
-  focusSession();
-  breakTimer();
+var start = document.getElementById('start');
+var stop = document.getElementById('stop');
+var reset = document.getElementById('reset');
+
+var wm = document.getElementById('w_minutes');
+var ws = document.getElementById('w_seconds');
+
+var bm = document.getElementById('b_minutes');
+var bs = document.getElementById('b_seconds');
+
+//store a reference to a timer variable
+var startTimer;
+
+start.addEventListener('click', function(){
+    if(startTimer === undefined){
+        startTimer = setInterval(timer, 1000)
+    } else {
+        alert("Timer is already running");
+    }
+})
+
+reset.addEventListener('click', function(){
+    wm.innerText = 25;
+    ws.innerText = "00";
+
+    bm.innerText = 5;
+    bs.innerText = "00";
+
+    document.getElementById('counter').innerText = 0;
+    stopInterval()
+    startTimer = undefined;
+})
+
+stop.addEventListener('click', function(){
+    stopInterval()
+    startTimer = undefined;
+})
+
+
+//Start Timer Function
+function timer(){
+    //Work Timer Countdown
+    if(ws.innerText != 0){
+        ws.innerText--;
+    } else if(wm.innerText != 0 && ws.innerText == 0){
+        ws.innerText = 59;
+        wm.innerText--;
+    }
+
+    //Break Timer Countdown
+    if(wm.innerText == 0 && ws.innerText == 0){
+        if(bs.innerText != 0){
+            bs.innerText--;
+        } else if(bm.innerText != 0 && bs.innerText == 0){
+            bs.innerText = 59;
+            bm.innerText--;
+        }
+    }
+
+    //Increment Counter by one if one full cycle is completed
+    if(wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0){
+        wm.innerText = 25;
+        ws.innerText = "00";
+
+        bm.innerText = 5;
+        bs.innerText = "00";
+
+        document.getElementById('counter').innerText++;
+    }
 }
 
-function start() {
-  click.play();
-
-  minutes = 24;
-  seconds = 59;
-
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
-
-  var minutesInterval = setInterval(minutesTimer, 60000);
-  var secondsInterval = setInterval(secondsTimer, 1000);
-
-  var focusInterval = setInterval(focusSession, 25 * 60000);
-  var breakInterval = setInterval(breakTimer, 5 * 60000);
-
-  function focusSession() {
-    if (minutes <= 0) {
-      console.log("session done");
-      minutes = 5;
-      breakTimer();
-      var done = true;
-    }
-  }
-
-  function breakTimer() {
-    if (minutes <= 0) {
-      console.log("break over");
-      minutes = 25;
-      focusSession();
-      var done = false;
-    }
-  }
-
-  function minutesTimer() {
-      var initialtime = minutes;
-    minutes--;
-    document.getElementById("minutes").innerHTML = minutes;
-    if (minutes <= 0) {
-      bell.play();
-if (initialtime === 24){
-    minutes = 5 ;
-    
-}else{
-    minutes = 25;
-}
-     
-    }
-  }
-
-  function secondsTimer() {
-    seconds--;
-    document.getElementById("seconds").innerHTML = seconds;
-    if (seconds <= 00) {
-      seconds = 60;
-    }
-  }
+//Stop Timer Function
+function stopInterval(){
+    clearInterval(startTimer);
 }
